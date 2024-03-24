98557df33880909819812071cbbeea210e585f3b
changuii <rhljh7410@gmail.com>
2024-03-24 18:41:27 +0900

    spring project build test

--- src/main/java/dev/test/test/TestController.java
+++ src/main/java/dev/test/test/TestController.java
@@ -1,3 +1,8 @@
+package dev.test.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class TestController {
     
